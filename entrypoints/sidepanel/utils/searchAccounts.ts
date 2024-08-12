a06228693d62b755596095cc4129ec1021ba9bb6
Callum <callum.mcintyre@anza.xyz>
2024-08-12 19:20:10 +0100

    Support searching accounts by (partial) address

--- entrypoints/sidepanel/utils/searchAccounts.ts
+++ entrypoints/sidepanel/utils/searchAccounts.ts
@@ -1,10 +1,14 @@
-import { SavedAccount } from "../../accounts/savedAccount";
+import { SavedAccount } from "../../../accounts/savedAccount";
 
 // TODO: should this be using locale compare?
 export function searchAccounts(accounts: SavedAccount[], searchQuery: string) {
+  const queryLowercase = searchQuery.toLowerCase();
+
   return accounts.filter(
     (account) =>
-      account.label.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      account.notes.toLowerCase().includes(searchQuery.toLowerCase())
+      account.label.toLowerCase().includes(queryLowercase) ||
+      account.notes.toLowerCase().includes(queryLowercase) ||
+      // addresses are case sensitive, but might be useful to search insensitively
+      account.address.toLowerCase().includes(queryLowercase)
   );
 }
