9b7b89d1d6d01718b2e08c71650f52796b63f54c
Callum <callum.mcintyre@anza.xyz>
2025-10-13 14:01:04 +0100

    Refactor ViewAccount page to use Mantine

--- entrypoints/sidepanel/components/AccountDisplay.tsx
+++ entrypoints/sidepanel/components/AccountDisplay.tsx
@@ -3,7 +3,7 @@ import { loader as filteredAccountsLoader } from '../routes/FilteredAccounts'
 import { shortAddress } from "../utils/address";
 import CopyButton from "./CopyButton";
 import TagBadge from "./TagBadge";
-import classes from "./AccountDisplay.module.css";
+import classes from "./HoverListItem.module.css";
 
 interface Props {
     account: Awaited<ReturnType<typeof filteredAccountsLoader>>['accounts'][0]
@@ -11,7 +11,7 @@ interface Props {
 
 export default function AccountDisplay({ account }: Props) {
     return (
-        <Stack gap="xs" className={classes.account}>
+        <Stack gap="xs" className={classes.listitem}>
             <Group gap="xs">
                 <Text span size="lg" >{account.label}</Text>
                 <Tooltip multiline maw="95%" label={account.address}>
@@ -19,7 +19,7 @@ export default function AccountDisplay({ account }: Props) {
                 </Tooltip>
                 <CopyButton address={account.address} />
             </Group>
-            <Text size="sm">{account.notes.split('\n').join(', ')}</Text>
+            <Text size="sm">{account.notes.split('\n').filter(n => n.trim() !== '').join(', ')}</Text>
             <Group gap="xs">
                 {account.tags.map(tag =>
                     <TagBadge key={tag} isFilled={false} isDisabled={false} color="gray.2">
--- entrypoints/sidepanel/routes/ViewAccount.tsx
+++ entrypoints/sidepanel/routes/ViewAccount.tsx
@@ -1,13 +1,14 @@
 import { getAccount } from "@/accounts/storage";
-import { Box, Button, ButtonGroup, Flex, Heading, HStack, IconButton, Image, Link, Skeleton, SkeletonCircle, SkeletonText, Spacer, Stack, Tag, Text, VStack, Wrap, WrapItem } from "@chakra-ui/react";
+import { Box, Button, Group, Title, Stack, Skeleton, Flex, Text, Badge, Avatar } from "@mantine/core";
+import { IconArrowLeft, IconEdit, IconSearch, IconCircleCheck, IconInnerShadowBottomRightFilled } from "@tabler/icons-react";
 import { Address } from "@solana/addresses";
 import React, { useCallback, useMemo } from "react";
 import { Link as ReactRouterLink, LoaderFunctionArgs, useLoaderData, defer, Await, useNavigate } from "react-router-dom";
 import { getTokensForAddress, TokenData } from "../utils/tokenData";
 import { SavedAccount } from "@/accounts/savedAccount";
-import { AddIcon, ArrowBackIcon, CheckCircleIcon, DeleteIcon, EditIcon, SearchIcon } from "@chakra-ui/icons";
 import { formatDollars, formatNumber, formatPercent } from "../utils/numberFormatter";
 import CopyButton from "../components/CopyButton";
+import classes from "../components/HoverListItem.module.css"
 
 interface Params {
     address: Address
@@ -21,9 +22,7 @@ type DeferredLoaderData = {
 export async function loader({ params }: LoaderFunctionArgs) {
     const { address } = params as unknown as Params
     const account = await getAccount(address);
-
     const tokensData = getTokensForAddress(address);
-
     return defer({ account, tokensData } as DeferredLoaderData);
 }
 
@@ -36,63 +35,73 @@ export default function ViewAccount() {
     }, [navigate]);
 
     return (
-        <VStack spacing={2} alignItems='stretch'>
-            <ButtonGroup spacing={4}>
-                <Button leftIcon={<ArrowBackIcon />} colorScheme='blue' variant='outline' onClick={handleBack}>
+        <Stack gap="xs">
+            <Group gap="md">
+                <Button leftSection={<IconArrowLeft size={16} />} variant='outline' onClick={handleBack}>
                     Back
                 </Button>
                 <ReactRouterLink to={`/accounts/${account.address}/edit`}>
-                    <Button leftIcon={<EditIcon />} colorScheme='blue' variant='outline'>
+                    <Button leftSection={<IconEdit size={16} />} variant='outline'>
                         Edit
                     </Button>
                 </ReactRouterLink>
                 <ReactRouterLink to={`https://explorer.solana.com/address/${account.address}`} target="_blank" rel="noopener noreferrer">
-                    <Button leftIcon={<SearchIcon />} colorScheme='blue' variant='outline'>
+                    <Button leftSection={<IconSearch size={16} />} variant='outline'>
                         Explorer
                     </Button>
                 </ReactRouterLink>
-            </ButtonGroup>
+            </Group>
 
-            <HStack spacing={2}>
-                <Heading as='h1' size='xl' noOfLines={1}>{account.label}</Heading>
+            <Group gap="sm">
+                <Title order={1} lineClamp={1}>{account.label}</Title>
                 <CopyButton address={account.address} />
-            </HStack>
+            </Group>
 
+            <Text size="sm" c="gray.3" style={{ wordBreak: 'break-all' }}>{account.address}</Text>
 
-            <Text fontSize='sm' color='gray.400' wordBreak='break-all'>{account.address}</Text>
+            <Title order={2} size="h3" mt="md">Notes</Title>
+            {account.notes.trim() !== '' ? (
+                <Text
+                    size="sm"
+                    c="gray.2"
+                    p="sm"
+                    style={{
+                        whiteSpace: 'pre-wrap',
+                        border: '1px solid var(--mantine-color-default-border)',
+                        borderRadius: 'var(--mantine-radius-default)'
+                    }}
+                >
+                    {account.notes}
+                </Text>
+            ) : null}
 
-            <Heading as='h2' size='md' marginTop={4}>Notes</Heading>
-            <Text whiteSpace='pre-wrap' fontSize='sm' color='gray.300' padding={2} borderRadius={2} borderStyle='solid' borderWidth={1}>{account.notes}</Text>
-
-            <Heading as='h2' size='md' marginTop={4}>Tags</Heading>
-            <Wrap>
+            <Title order={2} size="h3" mt="md">Tags</Title>
+            <Group gap="xs">
                 {account.tags.map(tag =>
-                    <WrapItem key={tag}>
-                        <Tag variant='outline' size='sm' key={tag}>{tag}</Tag>
-                    </WrapItem>
+                    <Badge variant='outline' size='sm' key={tag}>{tag}</Badge>
                 )}
-            </Wrap>
+            </Group>
 
-            <Heading as='h2' size='md' marginTop={4}>Tokens</Heading>
+            <Title order={2} size="h3" mt="md">Tokens</Title>
 
             <React.Suspense fallback={
-                <VStack width='100%' marginTop={1} spacing={4} alignItems='stretch'>
+                <Stack w="100%" mt="xs" gap="md">
                     {[...Array(4)].map((_, idx) => (
                         <TokenDataSkeleton key={idx} />
                     ))}
-                </VStack>
+                </Stack>
             }>
                 <Await resolve={tokensData}>
                     {(data: TokenData[]) => (
-                        <VStack width='100%' marginTop={1} spacing={0} alignItems='stretch'>
+                        <Stack w="100%" gap={0}>
                             {data.map(token => (
                                 <TokenDataDisplay key={token.mint} tokenData={token} />
                             ))}
-                        </VStack>
+                        </Stack>
                     )}
                 </Await>
             </React.Suspense>
-        </VStack>
+        </Stack>
     )
 }
 
@@ -112,27 +121,46 @@ function TokenDataDisplay({ tokenData }: { tokenData: TokenData }) {
     }, [tokenData.amount, tokenData.decimals]);
 
     return (
-        <Box paddingX={4} marginX={-4} paddingY={4} _hover={{ backgroundColor: 'gray.700' }}>
-            <Flex alignItems='center'>
-                {tokenData.icon ? (
-                    <Image src={tokenData.icon} alt={`${tokenData.name} icon`} boxSize='32px' borderRadius='full' marginRight={2} />
-                ) : null}
-                <Box>
-                    <HStack spacing={2}>
-                        <Text fontSize='sm' fontWeight='semibold'>{tokenData.name}</Text>
-                        {tokenData.jupiterIsVerified ? <CheckCircleIcon color='green.500' /> : null}
-                    </HStack>
-                    <HStack spacing={1}>
-                        <Text fontSize='sm' color='gray.400'>{usdPriceUnitFormatted}</Text>
-                        {tokenData.priceChange24hPercent ? (
-                            <Text fontSize='xs' color={tokenData.priceChange24hPercent >= 0 ? 'green.400' : 'red.400'}>({formatPercent(tokenData.priceChange24hPercent)})</Text>
-                        ) : null}
-                    </HStack>
-                </Box>
-                <Spacer />
-                <Box textAlign='right'>
-                    <Text fontSize='sm' fontWeight='semibold'>{usdValueFormatted}</Text>
-                    <Text fontSize='sm' color='gray.400'>{tokenAmountFormatted} {tokenData.symbol}</Text>
+        <Box
+            px="md"
+            mx="-md"
+            py="md"
+            style={{
+                cursor: 'pointer',
+                '&:hover': {
+                    backgroundColor: 'var(--mantine-color-dark-6)'
+                }
+            }}
+            className={classes.listitem}
+        >
+            <Flex align="center" justify="space-between">
+                <Flex align="center" gap="sm">
+                    {tokenData.icon ? (
+                        <Avatar src={tokenData.icon} alt={`${tokenData.name} icon`} size={32} radius="xl">
+                            <IconInnerShadowBottomRightFilled size={16} />
+                        </Avatar>
+                    ) : null}
+                    <Box>
+                        <Group gap="xs">
+                            <Text size="sm" fw={600}>{tokenData.name}</Text>
+                            {tokenData.jupiterIsVerified ? <IconCircleCheck size={16} color="var(--mantine-color-green-6)" /> : null}
+                        </Group>
+                        <Group gap="xs">
+                            <Text size="sm" c="dimmed">{usdPriceUnitFormatted}</Text>
+                            {tokenData.priceChange24hPercent ? (
+                                <Text
+                                    size="xs"
+                                    c={tokenData.priceChange24hPercent >= 0 ? 'green' : 'red'}
+                                >
+                                    ({formatPercent(tokenData.priceChange24hPercent)})
+                                </Text>
+                            ) : null}
+                        </Group>
+                    </Box>
+                </Flex>
+                <Box style={{ textAlign: 'right' }}>
+                    <Text size="sm" fw={600}>{usdValueFormatted}</Text>
+                    <Text size="sm" c="dimmed">{tokenAmountFormatted} {tokenData.symbol}</Text>
                 </Box>
             </Flex>
         </Box>
@@ -141,12 +169,12 @@ function TokenDataDisplay({ tokenData }: { tokenData: TokenData }) {
 
 function TokenDataSkeleton() {
     return (
-        <HStack gap="5">
-            <SkeletonCircle size="12" />
-            <Stack flex="1">
-                <Skeleton height="5" />
-                <Skeleton height="5" />
+        <Group gap="md">
+            <Skeleton height={48} circle />
+            <Stack flex="1" gap="xs">
+                <Skeleton c="gray.5" height={20} />
+                <Skeleton c="gray.5" height={20} />
             </Stack>
-        </HStack>
+        </Group>
     )
 }
