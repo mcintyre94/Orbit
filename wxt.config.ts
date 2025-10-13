0684159eaa08b18e7ced941dcb4de86f57093e52
Callum <callum.mcintyre@anza.xyz>
2025-10-13 19:22:18 +0100

    Clean up wxt config

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,31 +1,19 @@
 import { defineConfig } from "wxt";
 import react from "@vitejs/plugin-react";
-import { Plugin } from "vite";
-
-// web3js currently includes `process["env"].NODE_ENV
-// we switch this at build time to the Vite mode (typically development or production)
-function replaceProcessEnv(mode: string): Plugin {
-  const nodeEnvRegex = /process(\.env(\.NODE_ENV)|\["env"\]\.NODE_ENV)/g;
-  return {
-    name: "replace-process-env",
-    renderChunk(code) {
-      return code.replace(nodeEnvRegex, JSON.stringify(mode));
-    },
-  };
-}
+import { PluginOption } from "vite";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  vite: ({ mode }) => {
+  vite({ mode }): { plugins: PluginOption[] } {
     console.log("\nbuilding in mode", mode);
     return {
-      plugins: [react(), replaceProcessEnv(mode)],
+      plugins: [react()],
     };
   },
   manifest: {
     name: "Orbit",
     description: "Organise your Solana wallets",
-    version: "1.1.2",
+    version: "1.1.3",
     web_accessible_resources: [
       {
         matches: ["<all_urls>"],
